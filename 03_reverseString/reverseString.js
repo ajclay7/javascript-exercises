934ab523e8e49d55d611213737b0338156107712
Anthony <ajclay7@gmail.com>
2021-10-28 15:09:45 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
+    let strArray = Array.from(str);
 
+    strArray = strArray.reverse();
+
+    return strArray.join("");
 };
 
 // Do not edit below this line
